e3c0f9dbdea2ed44ab73adb3034b9ba54ce0075d
18790391230 <751586981@qq.com>
2020-11-21 20:51:20 +0800

    希尔排序移位法

--- src/main/java/com/wym/sort/ShellSort.java
+++ src/main/java/com/wym/sort/ShellSort.java
@@ -12,6 +12,35 @@ public class ShellSort {
         for (int i = 10; i > 0; i--) {
             arr[index++] = i;
         }
+//        swap(arr);
+
+        move(arr);
+    }
+
+    private static void move(int[] arr) {
+
+        System.out.println("排序前==============");
+        System.out.println(Arrays.toString(arr));
+
+        int step = arr.length;
+
+        while ((step = step / 2) > 0) {  //当step == 1时，是最后一轮排序
+            for (int i = step; i < arr.length; i++) {  //这个循环每执行一次，相当于进行一次插入排序
+                int insertVal = arr[i];
+                int j = i;
+                while (j - step >= 0 && insertVal < arr[j - step]) {
+                    arr[j] = arr[j - step];
+                    j -= step;
+                }
+                arr[j] = insertVal;
+            }
+        }
+
+        System.out.println("排序后==============");
+        System.out.println(Arrays.toString(arr));
+    }
+
+    private static void swap(int[] arr) {
         System.out.println("排序前==============");
         System.out.println(Arrays.toString(arr));
 
@@ -19,7 +48,7 @@ public class ShellSort {
 
         int temp = 0;
         while ((step = step / 2) > 0) {
-            for (int i = step; i < arr.length; i++) {
+            for (int i = step; i < arr.length; i++) {  //这个循环每执行一次，相当于进行一次插入排序
                 for (int j = i - step; j >= 0; j-=step) {
                     if (arr[j] > arr[j + step]) {
                         temp = arr[j];
@@ -32,6 +61,5 @@ public class ShellSort {
 
         System.out.println("排序后==============");
         System.out.println(Arrays.toString(arr));
-
     }
 }
